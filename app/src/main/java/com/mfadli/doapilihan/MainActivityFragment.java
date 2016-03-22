26e001a254410a298cf7cfa029192c5e3575b0df
Fadli Ishak <fadliishak@gmail.com>
2016-03-22 21:52:07 +0800

    Add on touch listener to title recycler view.

--- app/src/main/java/com/mfadli/doapilihan/MainActivityFragment.java
+++ app/src/main/java/com/mfadli/doapilihan/MainActivityFragment.java
@@ -1,13 +1,17 @@
 package com.mfadli.doapilihan;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.mfadli.utils.Common;
 
@@ -44,7 +48,18 @@ public class MainActivityFragment extends Fragment {
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
         mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL));
         mRecyclerView.setAdapter(mMainAdapter);
+        mRecyclerView.addOnItemTouchListener(new RecyclerTouchListener(getContext(), mRecyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                String title = mList.get(position);
+                Toast.makeText(getContext(), title, Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public void onLongClick(View view, int position) {
+                Toast.makeText(getContext(), "long click", Toast.LENGTH_SHORT).show();
+            }
+        }));
         loadTitles();
 
         return view;
@@ -59,5 +74,59 @@ public class MainActivityFragment extends Fragment {
             mList.add(l);
         }
         mMainAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * ClickListener Interface
+     */
+    public interface ClickListener {
+        void onClick(View view, int position);
+
+        void onLongClick(View view, int position);
+    }
+
+    /**
+     * Recycler OnItemTouchListener class
+     */
+    public static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+        private GestureDetector mGestureDetector;
+        private MainActivityFragment.ClickListener mClickListener;
+
+        public RecyclerTouchListener(Context context, RecyclerView recyclerView, ClickListener clickListener) {
+            mClickListener = clickListener;
+            mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && mClickListener != null) {
+                        mClickListener.onLongClick(child, recyclerView.getChildPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && mClickListener != null && mGestureDetector.onTouchEvent(e)) {
+                mClickListener.onClick(child, rv.getChildPosition(child));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
     }
 }
